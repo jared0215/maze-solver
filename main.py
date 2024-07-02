4c125e3776c6396575de098cbee3926e7c914ae9
jared0215 <jared0215@hotmail.com>
2024-07-02 16:15:10 -0400

    Update methods to properly draw window on screen

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add the src directory to the Python path
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+
 from window import Window
 
 win = Window(800, 600)
